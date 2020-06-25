db647077e5659ab0d74874bb6f66ff86666b5b78
Renata Marques <45424559+RehMarNo@users.noreply.github.com>
2020-06-24 23:50:08 -0300

    Update EulerMelhorado.py

--- EulerMelhorado.py
+++ EulerMelhorado.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 def eulerMod(h, Tmax):
-  u = 8
+  u = 8       #condição inicial
   itMax = Tmax/h     #quantidade máxima de iterações
   for i in np.arange(1,itMax):
     t = (i-1)*h
-    k1 = (-0.5*u + 2 + t)
-    u_til = u + h*k1
-    k2 = (-0.5*u_til + 2 + t + h)
+    k1 = (-0.5*u + 2 + t)    #edo
+    u_Atualizado = u + h*k1
+    k2 = (-0.5*u_Atualizado + 2 + t + h)
     u= u + h * (k1 + k2)/2
     print("it: ", i, "|  u[i]: ",u)
 
